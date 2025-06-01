ac4470828e497242ae44f064493cd2087fa1210d
hSerein <1525070378@qq.com>
2025-06-01 21:01:24 +0800

    feat: 知乎问答api设计

---
+++ src/assets/js/request/AnswerAPI.js
@@ -0,0 +1,114 @@
+import axiosPlugin from '@/axiosPlugin.js'
+import { message } from 'ant-design-vue';
+import { errorHandler } from './common.js'
+import qs from "qs";
+import { baseURL } from '@/config.js'
+
+// 获取回答列表
+export async function gainAnswer(params) {
+    let data = null;
+    // console.log(params)
+    await axiosPlugin({
+        method: "GET",
+        url: baseURL + "/qaService/answer/all",
+        params: {
+            page: params.page,
+            pageSize: params.pageSize,
+        },
+    }).then((response) => {
+        console.log(response)
+        data = response.data.msg;
+        if (response.data.state === "SUCCESS") {
+            data = response.data.msg;
+            localStorage.setItem("AnswerInfoList", JSON.stringify(data));
+            message.success('回答列表获取成功');
+        }
+    // }).catch((response) => errorHandler(response));
+    }).catch((response) => {
+        console.log(response);
+    })
+    return data;
+}
+
+// 修改回答
+export async function updateAnswerPut(params) {
+    let data = null;
+    await axiosPlugin({
+        method: "PUT",
+        url: baseURL + "/qaService/answer/update",
+        params: {
+            answerId: params.answerId,
+            content: params.content,
+        },
+    }).then((response) => {
+        // console.log(response)
+        data = response.data.msg;
+        if (response.data.state === "SUCCESS") {
+            data = response.data.msg;
+            message.success('回答修改成功');
+        }
+    }).catch((response) => errorHandler(response));
+    return data;
+}
+
+// 删除回答
+export async function deleteAnswer(answerId) {
+    let data = null;
+    await axiosPlugin({
+        method: "DELETE",
+        url: baseURL + "/qaService/answer/delete",
+        params: {
+            answerId: answerId,
+        },
+    }).then((response) => {
+        console.log(response)
+        data = response.data.msg;
+        if (response.data.state === "SUCCESS") {
+            data = response.data.msg;
+            message.success('回答修改成功');
+        }
+    }).catch((response) => errorHandler(response));
+    return data;
+}
+
+// 上传图片接口
+export async function uploadImage(file) {
+    if (!file) {
+        message.error('请选择要上传的图片');
+        return null;
+    }
+    const maxSize = 1 * 1024 * 1024; // 1MB
+    if (file.size > maxSize) {
+      console.error('图片不能超过1MB');
+      message.error('图片不能超过1MB');
+      return;
+    }
+    if (!file.type.startsWith('image/')) {
+      console.error('只允许上传图片');
+      message.error('只允许上传图片');
+      return;
+    }
+    let data = null;
+
+    const formData = new FormData();
+    formData.append('uploadFile', file);
+
+    await axiosPlugin({
+        method: "POST",
+        url: baseURL + "/qaService/qaFile/uploadFile",
+        headers: {
+            'Content-Type': 'multipart/form-data'
+        },
+        data: formData,
+    }).then((response) => {
+        console.log(response)
+        data = response.data.msg;
+        if (response.data.state === "SUCCESS") {
+            data = response.data.msg.uploadId;
+            message.success('图片上传成功');
+        } else {
+            message.error(response.data.msg || '图片上传失败');
+        }
+    }).catch((response) => errorHandler(response));
+    return data;
+}
---
+++ src/assets/js/request/QuestionAPI.js
@@ -0,0 +1,173 @@
+import axiosPlugin from '@/axiosPlugin.js'
+import { message } from 'ant-design-vue';
+import { errorHandler } from './common.js'
+import qs from "qs";
+import { baseURL } from '@/config.js'
+
+// 搜索问题
+export async function searchByKeywords(params = {}) {
+    let data = null;
+    await axiosPlugin({
+        method: "GET",
+        url: baseURL + '/qaService/question/search',
+        params: {
+            keywords: params.keywords,
+            page: params.page,
+            rows: params.rows
+        },
+    }).then((response) => {
+        console.log(response)
+        let searchHits = response.data.msg.searchHits;
+        // let questionListTemp = searchHits.map((data) => {
+        //     if (data.highlightFields != null) {
+        //       if (data.highlightFields.abstractContent != null && data.highlightFields.abstractContent.length > 0) {
+        //         data.content.abstractContent = data.highlightFields.abstractContent[0];
+        //       }
+        //       if (data.highlightFields.title != null && data.highlightFields.title.length > 0) {
+        //         data.content.title = data.highlightFields.title[0];
+        //       }
+        //       if (data.highlightFields.content != null && data.highlightFields.content.length > 0) {
+        //         data.content.content = data.highlightFields.content[0];
+        //       }
+        //     }
+        //     return data.content;
+        //   });
+        let questionListTemp = searchHits.map(data => data.content);
+        data = questionListTemp;
+        // localStorage.setItem("token", data);
+        message.success('搜索成功');
+    }).catch((response) => errorHandler(response));
+    return data;
+}
+
+// 列表获取个人的或者所有问题
+export async function listByUsername(params = {}) {
+    let data = null;
+    if (params.username) {
+        await axiosPlugin({
+            method: "GET",
+            url: baseURL + "/qaService/question/list",
+            params: {
+                username: params.username,
+                page: params.page,
+                pageSize: params.pageSize,
+                sort: 0
+            },
+        }).then((response) => {
+            // console.log(params)
+            // console.log(response)
+            data = response.data.msg;
+            localStorage.setItem("QuestionInfoList", JSON.stringify(data));
+            message.success('问题查询成功');
+        }).catch((response) => errorHandler(response));
+    }
+    else {
+        await axiosPlugin({
+            method: "GET",
+            url: baseURL + "/qaService/question/list",
+            params: {
+                page: params.page,
+                pageSize: params.pageSize,
+                sort: 0
+            },
+        }).then((response) => {
+            // console.log(params)
+            // console.log(response)
+            data = response.data.msg;
+            localStorage.setItem("QuestionInfoList", JSON.stringify(data));
+            message.success('问题查询成功');
+        }).catch((response) => errorHandler(response));
+    }
+    return data;
+}
+
+// 删除问题
+export async function deleteQuestion(questionId) {
+    let data = null;
+    await axiosPlugin({
+        method: "DELETE",
+        url: baseURL + "/qaService/question/delete",
+        params: {
+            questionId: questionId,
+        },
+    }).then((response) => {
+        // console.log(response)
+        data = response.data.msg;
+        if (response.data.state === "SUCCESS") {
+            data = response.data.msg;
+            message.success('问题删除成功');
+        }
+    }).catch((response) => errorHandler(response));
+    return data;
+}
+
+// 修改/发布/隐藏问题
+export async function updateQuestion(params) {
+    let data = null;
+    await axiosPlugin({
+        method: "PUT",
+        url: baseURL + "/qaService/question/update",
+        headers: {
+            'Content-Type': 'multipart/form-data'
+        },
+        params: {
+            questionId: params.id,
+            // isPost: 1,
+            // isHide: 0,
+        },
+        data: {
+            title: params.title,
+            content: params.content
+        }
+    }).then((response) => {
+        console.log(response)
+        data = response.data.msg;
+        if (response.data.state === "SUCCESS") {
+            data = response.data.msg;
+            message.success('问题修改成功');
+        }
+    }).catch((response) => errorHandler(response));
+    return data;
+}
+
+// 上传图片接口
+export async function uploadImage(file) {
+    if (!file) {
+        message.error('请选择要上传的图片');
+        return null;
+    }
+    const maxSize = 1 * 1024 * 1024; // 1MB
+    if (file.size > maxSize) {
+      console.error('图片不能超过1MB');
+      message.error('图片不能超过1MB');
+      return;
+    }
+    if (!file.type.startsWith('image/')) {
+      console.error('只允许上传图片');
+      message.error('只允许上传图片');
+      return;
+    }
+    let data = null;
+
+    const formData = new FormData();
+    formData.append('uploadFile', file);
+
+    await axiosPlugin({
+        method: "POST",
+        url: baseURL + "/qaService/qaFile/uploadFile",
+        headers: {
+            'Content-Type': 'multipart/form-data'
+        },
+        data: formData,
+    }).then((response) => {
+        console.log(response)
+        data = response.data.msg;
+        if (response.data.state === "SUCCESS") {
+            data = response.data.msg.uploadId;
+            message.success('图片上传成功');
+        } else {
+            message.error(response.data.msg || '图片上传失败');
+        }
+    }).catch((response) => errorHandler(response));
+    return data;
+}
